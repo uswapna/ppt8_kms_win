65e2c43f6ba1fd3e7b00717c79b2df080003fd0e
uswapna <swapna.uplekar@alight.com>
2025-05-13 21:28:56 +0530

    Update init.pp
    is_domain_name is not working with stdlib used for puppet 8

--- manifests/init.pp
+++ manifests/init.pp
@@ -37,7 +37,7 @@ class kms_win (
 ){
 
   # parameter validation
-  unless is_domain_name($key_management_service_name) {
+  unless $key_management_service_name =~ Stdlib::Fqdn {
     fail('Class[kms_win] key_management_service_name parameter must be a valid rfc1035 domain name')
   }
   validate_re($key_management_service_port, '\d+', 'key_management_service_port parameter must be a number.')
